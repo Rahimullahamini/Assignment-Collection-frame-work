133e4c04faabd23aab9c544e65e74305c1f0a577
Rahimullahamini <103835973+Rahimullahamini@users.noreply.github.com>
2022-11-04 13:37:20 -0700

    vector
    vector is added

--- Test.java
+++ Test.java
@@ -1,52 +1,73 @@
 package pack;
 
-import java.util.ArrayList;
-import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.Vector;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		ArrayList list = new ArrayList();
-		list.add("FaizAhamd");
+//		vector class has 4 constructer.
+//		default constructer.
+		Vector list = new Vector();
+		list.add("faiz");
+		list.add(50000);
 		list.add("Ahmad");
-		list.add(5960);
+		list.add(50000);
 		
-//		Normal ListIterator.
+		System.out.println(list);
 		
-		ListIterator data = list.listIterator();
+//		for each loop.
+		for(Object obj: list){
+			Object o = (Object)obj;
+			System.out.println(o);
+		}
+		
+
+//		Iterator curser.
+//		user defined capacity constructer.
+		
+		Vector list01 = new Vector(5);
+		list01.add("faiz");
+		list01.add(50000);
+		list01.add("Ahmad");
+		list01.add(50000);
+		list01.add("AhmadKhan");
+		list01.add(50658);
+		list01.add("AhmadJan");
+		list01.add(5590);
+		
+		
+		Iterator data = list01.iterator();
 		while(data.hasNext()){
 			Object obj = (Object)data.next();
 			System.out.println(obj);
-			
 		}
-		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
-		while(data.hasPrevious()){
-			Object ob = (Object)data.previous();
+		System.out.println(list01.capacity());
+		
+		
+		
+//		3 constrcter in the vectoer classes.
+		
+		System.out.println("/////////////////////////////////////");
+		
+		Vector list02 = new Vector(5, 8);
+		list02.add("faiz");
+		list02.add(50000);
+		list02.add("Ahmad");
+		list02.add(50000);
+		list02.add("AhmadKhan");
+		list02.add(50658);
+		list02.add("AhmadJan");
+		list02.add(5590);
+	
+		
+		Iterator data01 = list02.iterator();
+		while(data01.hasNext()){
+			Object ob = (Object)data01.next();
 			System.out.println(ob);
-			
 		}
-		
-//		Genric ListIterator.
-		
-		ArrayList<student> listdata = new ArrayList<student>();
-		listdata.add(new student(1, "Faiz Ahmad Faiz"));
-		listdata.add(new student(2, "walid"));
-		
-		ListIterator<student> listIter = listdata.listIterator();
-		while(listIter.hasNext()){
-			student s = listIter.next();
-			System.out.println(s.sid + " : " + s.sName);
-		}
-		
-		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
-		
-		while(listIter.hasPrevious()){
-			student st = listIter.previous();
-			System.out.println(st.sid + " , " + st.sName);
-		}
-		
+		System.out.println(list02.capacity());
 		
 	}
-
 }
